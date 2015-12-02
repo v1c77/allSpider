3de89bca48635abe33108ba6970153b328dd5f26
diaoshe <heyuahude@gmail.com>
2015-12-02 23:50:17 +0800

    添加时间戳精细化管理

--- autoTime.py
+++ autoTime.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 __author__ = 'diaoshe'
 "获取时间字符串并返回规范化datetime"
 import re
@@ -7,105 +7,92 @@ import datetime
 
 now = datetime.datetime.now()
 
+
 def strToTime(Timestr):
     "多格式输入 正则表达式进行逻辑匹配,返回统一时间格式"
     if isinstance(Timestr, unicode):
         "统一编码格式"
         Timestr = Timestr.encode('utf-8')
-        pat1 = r'\d{4}年\d{2}月\d{2}日\d{2}:\d{2}'
-        pattern1 = re.compile(pat1)
-        match1 = pattern1.match(Timestr)
+    pat1 = r'\d{4}年\d{2}月\d{2}日\d{2}:\d{2}'
+    pattern1 = re.compile(pat1)
+    match1 = pattern1.match(Timestr)
 
+    if match1:
 
-        if match1 :
+        unifidtime = datetime.datetime.strptime(Timestr, '%Y年%m月%d日%H:%M')
+        return unifidtime
+    else:
+        pat2 = r'(\d{1,2})小时前'
+        pattern2 = re.compile(pat2)
+        match2 = pattern2.match(Timestr)
+        if match2:
 
-
-            unifidtime = datetime.datetime.strptime(Timestr,'%Y年%m月%d日%H:%M')
+            hourstr = int(match2.group(1))
+            hour = datetime.timedelta(hours=hourstr)
+            unifidtime = now - hour
             return unifidtime
-        else:
-            pat2 = r'(\d{1,2})小时前'
-            pattern2 = re.compile(pat2)
-            match2 = pattern2.match(Timestr)
-            if match2:
 
-                hourstr = int(match2.group(1))
-                hour = datetime.timedelta(hours=hourstr)
-                unifidtime = now - hour
+
+        else:
+            pat3 = r'(\d{1,2})分钟前'
+            pattern3 = re.compile(pat3)
+            match3 = pattern3.match(Timestr)
+            if match3:
+
+                minstr = int(match3.group(1))
+                minutes = datetime.timedelta(minutes=minstr)
+                unifidtime = now - minutes
                 return unifidtime
-
-
             else:
-                pat3 = r'(\d{1,2})分钟前'
-                pattern3 = re.compile(pat3)
-                match3 = pattern3.match(Timestr)
-                if match3:
-
-                    minstr = int(match3.group(1))
-                    minutes = datetime.timedelta(minutes=minstr)
-                    unifidtime = now - minutes
-                    return unifidtime
-                else:
-                    print "呜呜~没有找到~"
+                print "呜呜~没有找到~"
 
 
-
-
-
-
-
-if __name__=="__main__":
-    "测试"
-    date=['2015年11月18日17:58','6小时前','37分钟前','7小时前','14小时前',u'2015\u5e7411\u670818\u65e515:30','2015年11月18日15:39',u'4\u5c0f\u65f6\u524d','3小时前']
-    print '现在时间',now
-
-
-
-
-    #匹配文本获得匹配结果  无法匹配则返回None
-    for each in date:
-
-        if isinstance(each, unicode):
-            each = each.encode('utf-8')
-
-
-        pat1 = r'\d{4}年\d{2}月\d{2}日\d{2}:\d{2}'
-        pattern1 = re.compile(pat1)
-        match1 = pattern1.match(each)
-        print each
-
-        if match1 :
-
-            print 'get年月日！'
-            unifidtime = datetime.datetime.strptime(each,'%Y年%m月%d日%H:%M')
-            print unifidtime
-        else:
-            pat2 = r'(\d{1,2})小时前'
-            pattern2 = re.compile(pat2)
-            match2 = pattern2.match(each)
-            if match2:
-                print 'get小时！'
-                hourstr = int(match2.group(1))
-                hour = datetime.timedelta(hours=hourstr)
-                unifidtime = now - hour
-                print unifidtime
-
-
-            else:
-                pat3 = r'(\d{1,2})分钟前'
-                pattern3 = re.compile(pat3)
-                match3 = pattern3.match(each)
-                if match3:
-                    print 'get分钟！'
-                    minstr = int(match3.group(1))
-                    minutes = datetime.timedelta(minutes=minstr)
-                    unifidtime = now - minutes
-                    print unifidtime
-                else:
-                    print "呜呜～～"
-
-
-
-
-
-
-
+# if __name__ == "__main__":
+#     "测试"
+#     date = ['2015年11月18日17:58', '6小时前', '37分钟前', '7小时前', '14小时前', u'2015\u5e7411\u670818\u65e515:30',
+#             '2015年11月18日15:39', u'4\u5c0f\u65f6\u524d', '3小时前']
+#     print '现在时间', now
+#
+#
+#
+#
+#     # 匹配文本获得匹配结果  无法匹配则返回None
+#     for each in date:
+#
+#         if isinstance(each, unicode):
+#             each = each.encode('utf-8')
+#
+#         pat1 = r'\d{4}年\d{2}月\d{2}日\d{2}:\d{2}'
+#         pattern1 = re.compile(pat1)
+#         match1 = pattern1.match(each)
+#         print each
+#
+#         if match1:
+#
+#             print 'get年月日！'
+#             unifidtime = datetime.datetime.strptime(each, '%Y年%m月%d日%H:%M')
+#             print unifidtime
+#         else:
+#             pat2 = r'(\d{1,2})小时前'
+#             pattern2 = re.compile(pat2)
+#             match2 = pattern2.match(each)
+#             if match2:
+#                 print 'get小时！'
+#                 hourstr = int(match2.group(1))
+#                 hour = datetime.timedelta(hours=hourstr)
+#                 unifidtime = now - hour
+#                 print unifidtime
+#
+#
+#             else:
+#                 pat3 = r'(\d{1,2})分钟前'
+#                 pattern3 = re.compile(pat3)
+#                 match3 = pattern3.match(each)
+#                 if match3:
+#                     print 'get分钟！'
+#                     minstr = int(match3.group(1))
+#                     minutes = datetime.timedelta(minutes=minstr)
+#                     unifidtime = now - minutes
+#                     print unifidtime
+#                 else:
+#                     print "呜呜～～"
--- baiduNews.py
+++ baiduNews.py
@@ -6,19 +6,52 @@ import requests
 import os
 import IOmymongo
 import autoTime
+import datetime
+import time
 
 from multiprocessing.dummy import Pool as ThreadPool
-
 
 keyword = '五中全会'
 pn = 0
 # 新闻/全部
+baidu = 'http://news.baidu.com/ns?cl=2&word=%s&pn=' % keyword + str(pn)
 
 # baidu = 'http://www.baidu.com/s?wd=%s&pn=' % keyword + str(pn)
-baidu = 'http://news.baidu.com/ns?word=%s&rn=50&pn=' % keyword + str(pn)
+# baidu = 'http://news.baidu.com/ns?word=%s&pn=' % keyword + str(pn)
+
+# 精细化时间
+starttime = datetime.datetime(2015, 11, 1)     #起始查询时间
+deartetime = datetime.timedelta(seconds=58, minutes=59, hours=23)
+oneday = datetime.timedelta(days=1)
+stoptime = int(time.mktime(datetime.datetime.now().timetuple()))    #截止时间
+endtime = starttime + deartetime
+str_start_time = int(time.mktime(starttime.timetuple()))
+str_end_time = str_start_time + 86399
+
+gpc = '&gpc=stf%3D' + str(str_start_time) + '%2C' + str(str_end_time) + '%7Cstftype%3D2'
+
+baidu = baidu + gpc
+#      &gpc=stf%3D1446307200%2C1448899200%7Cstftype%3D2
 
 
 
+# stf = 'stf=' + str(str_start_time) + ',' + str(str_end_time) + '|stftype=2'  # 时间戳
+
+# dates = {
+#     'ie': 'utf-8',
+#     # 'f':'8', ##no
+#     # 'rsv_bp':'1',  #no
+#     'rsv_idx': '2',  # 一样
+#     'tn': '97272809_hao_pg',
+#     'wd': keyword,  # 必须
+#     'rsv_spt': '1',
+#     'oq': keyword,  # 必须
+#     # 'rsv_pq':'f45502e700019c1a',   #每次不一样 去掉
+#     # 'rsv_t':'d782WhjQfs9J36DrGeC6UejCMJDlygbA/B+c7pmnYlhWKPIuBDnprwiknKCLhdPu70XIFbvb' #不一样
+#     # 'rsv_enter':'1'  #no
+#     'gpc': stf,  # 时间戳
+#     'tfflag': '1'
+# }
 
 headers = {
 
@@ -28,33 +61,36 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
     'Accept-Encoding': 'gzip, deflate'
 }
-def towritetxt(contentdict):
-    "文本存储"
-    f.writelines(u'发表时间：' + str(contentdict['topic_time']) + '\n')
-    f.writelines(u'内容：' + str(contentdict['topic_content']) + '\n')
-    f.writelines(u'发帖人：' + str(contentdict['user_name']) + '\n\n')
+
+
+# def towritetxt(contentdict):
+#     "文本存储"
+#     f.writelines(u'发表时间：' + str(contentdict['topic_time']) + '\n')
+#     f.writelines(u'内容：' + str(contentdict['topic_content']) + '\n')
+#     f.writelines(u'发帖人：' + str(contentdict['user_name']) + '\n\n')
 
 
 def getUrlTime(url):
-
     # trueUrl = requests.get(url)
     # print trueUrl.url
 
     # print url
     r = requests.get(url)
     # print r.headers
-    #通过捕获错误判断时间
+    # 通过捕获错误判断时间
     try:
 
         return r.headers['Last-Modified']
     except KeyError:
         return r.headers['date']
 
+
 class baiduNewsSpider(object):
     "找呀找呀找朋友。。。"
+
     def GetSource(self):
-        source = requests.get(baidu,headers=headers)
-        if source.status_code == 503: # Why did this happen?
+        source = requests.get(baidu, headers=headers)
+        if source.status_code == 503:  # Why did this happen?
             print("[!] An error occurred! Please check whether any verification code")
             os._exit(0)
 
@@ -73,12 +109,12 @@ class baiduNewsSpider(object):
         print numstr
         number = filter(str.isalnum, numstr.encode("utf-8"))
         s = float(number)
-        Num = int(round(s / 10)) # Calculation about pages
+        Num = int(round(s / 10))  # Calculation about pages
         print("[+] Results there are %s pages, there are about %s results" % (Num, number))
-        Pages = Num * 10 - 10 # Calculation about page number
+        Pages = Num * 10 - 10  # Calculation about page number
         return Pages
 
-    def getBox(self,entire):
+    def getBox(self, entire):
         selector = etree.HTML(entire)
         box_field = selector.xpath('//div[@class="result"]')
         return box_field
@@ -93,7 +129,7 @@ class baiduNewsSpider(object):
         a = ''
         strAll = a.join(Str)
 
-        #时间的精细化处理
+        # 时间的精细化处理
         source = Time.split()[0]
         Time = Time.split()[1:]
         Time = a.join(Time)
@@ -107,36 +143,47 @@ class baiduNewsSpider(object):
         return objitem
 
 
-
 if __name__ == "__main__":
     # data['q'] = raw_input('Enter what you want:')
+    print("[!] Start run  BaiduNewsSpider...")
     spider = baiduNewsSpider()
     mongocont = IOmymongo.conToMogd()
-    print("[!] Start run  BaiduNewsSpider...")
+
     # bnContion = pymongo.Connection
     # text = spider.GetSource() # Get Sources
-    Pages = spider.GetPages(spider.GetSource()) # To calculate Pages
-    f = open('result.txt', 'a')
-    p = 1
-    while pn <= Pages:
-        print("[+] Start scraping content... The %d page" % p)
-        p += 1
-        text = spider.GetSource() #获取百度搜索页面数据
-        objOfPages = spider.getBox(text)
-        print '[+] Geting source...'
+    while str_end_time < stoptime:
+        print '[+]Start a part spdider'
+        Pages = spider.GetPages(spider.GetSource())  # To calculate Pages
+        p = 1
+        pn = 0
+        baidu = 'http://news.baidu.com/ns?cl=2&word=%s&pn=' % keyword + str(pn) + '&gpc=stf%3D' + str(str_start_time) + '%2C' + str(str_end_time) + '%7Cstftype%3D2'
+        while pn <= Pages:
+            print("[+] Start scraping content... The %d page" % p)
+            p += 1
+            text = spider.GetSource()  # 获取百度搜索页面数据
+            objOfPages = spider.getBox(text)
+            print '[+] Geting source...'
 
-        for each in objOfPages:
-            testobj = spider.GetUrlStrTime(each)
-            "输出数据"
-            print '*url:',testobj['Url'],',','Title:',testobj['Str'],',','Time:',testobj['Time']
-            IOmymongo.writeToMongo(mongocont,testobj)
+            for each in objOfPages:
+                testobj = spider.GetUrlStrTime(each)
+                "输出数据"
+                print '*url:', testobj['Url'], ',', 'Title:', testobj['Str'], ',', 'Time:', testobj['Time']
+                IOmymongo.writeToMongo(mongocont, testobj)
 
-        #多线程分析
+            # 多线程分析
+
+            # 重新加载数据项
+            pn = pn + 10
+
+            baidu = 'http://news.baidu.com/ns?cl=2&word=%s&pn=' % keyword + str(pn) + '&gpc=stf%3D' + str(str_start_time) + '%2C' + str(str_end_time) + '%7Cstftype%3D2'
 
 
-        pn = pn + 10
+        #修改精细时间
+        str_start_time = str_start_time + 86400
+        str_end_time = str_start_time + 86399
+
         # 新闻/全部
-        # baidu = 'http://www.baidu.com/s?wd=%s&pn=' % keyword + str(pn)
-        baidu = 'http://news.baidu.com/ns?word=%s&rn=50&pn=' % keyword + str(pn)
-    #     time.sleep(2) # To prevent Google verification code, dormancy for 30 seconds
-    # f.close()
+
+        # baidu = 'http://news.baidu.com/ns?word=%s&rn=50&pn=' % keyword + str(pn)
+        #     time.sleep(2) # To prevent Google verification code, dormancy for 30 seconds
+        # f.close()
